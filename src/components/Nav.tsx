b27fe716437417ac480d8e289af16f8d071e45f3
Marina <pchupchu@yandex.ru>
2024-12-05 10:31:30 +0300

    Add Nav component

---
+++ src/components/Nav.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { Link } from 'react-scroll';
+
+interface NavProps {
+  containerStyle?: string;
+  listStyle?: string;
+  linkStyle?: string;
+  spy?: boolean;
+}
+
+const links = [
+  { name: 'home' },
+  { name: 'about' },
+  { name: 'journey' },
+  { name: 'work' },
+  { name: 'contact' },
+];
+
+const Nav = ({ containerStyle, listStyle, linkStyle, spy }: NavProps) => {
+  return (
+    <nav className={containerStyle}>
+      <ul className={listStyle}>
+        {links.map((link, index) => (
+          <Link
+            className={linkStyle}
+            key={index}
+            to={link.name}
+            spy={spy}
+            activeClass='active'
+            smooth>
+            {link.name}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
