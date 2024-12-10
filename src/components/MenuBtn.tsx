e4cbbdff29b92be5293ea7be80cf0487257cfeca
Marina <pchupchu@yandex.ru>
2024-12-10 13:47:48 +0300

    Add prop onClick to MenuBtn

--- src/components/MenuBtn.tsx
+++ src/components/MenuBtn.tsx
@@ -2,12 +2,14 @@ import { CgMenuGridR } from 'react-icons/cg';
 
 interface MenuBtnProps {
   btnClass?: string;
+  onClick?: () => void;
 }
 
-const MenuBtn = ({ btnClass }: MenuBtnProps) => {
+const MenuBtn = ({ btnClass, onClick }: MenuBtnProps) => {
   return (
     <button
-      className={`bg-accent shadow-custom flex h-[54px] w-[54px] cursor-pointer select-none items-center justify-center rounded-lg ${btnClass}`}>
+      className={`bg-accent shadow-custom flex h-[54px] w-[54px] cursor-pointer select-none items-center justify-center rounded-lg ${btnClass}`}
+      onClick={onClick}>
       <CgMenuGridR className='text-4xl text-white' />
     </button>
   );
